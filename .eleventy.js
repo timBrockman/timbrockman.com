4db477cfb17f7b222ec97f18d2dc973aa4bdfd96
Tim Brockman <timBrockman@users.noreply.github.com>
2023-06-05 21:41:06 +0000

    updated config, plugin adds

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,24 @@
+/* eleventy config file
+    - add plugins
+    - add directories
+    
+*/
+
+// import plugins from package.json
+const pluginImage = require("@11ty/eleventy-img");
+//const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginAmp = require("@ampproject/eleventy-plugin-amp");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig){
+    //plugins
+    eleventyConfig.addPlugin(pluginNavigation);
+    eleventyConfig.addPlugin(pluginRss);
+    //eleventyConfig.addPlugin(pluginSyntaxHighlight);
+    eleventyConfig.addPlugin(pluginAmp,{ imageOptimization: true });
+    eleventyConfig.addPlugin(pluginImage);   
+
     //directories and such
     return{
         dir: {
